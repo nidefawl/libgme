dab807b3bba3f0e660f978e072f09f13c15634ad
James Dunne <james.jdunne@gmail.com>
2018-11-25 00:16:06 -0600

    Offset pitches down 3 octaves.

--- gme/Spc_Dsp.h
+++ gme/Spc_Dsp.h
@@ -204,7 +204,7 @@ public:
 
 			double scale = pitch / (double)0x1000;
 			// double m = (log(hz) / log(2)) * 12 + melodic_note;
-			double m = ((log(base_pitch * scale) / log(2)) * 12);
+			double m = ((log(base_pitch * scale) / log(2)) * 12) - 36;
 
 			return m;
 		}
